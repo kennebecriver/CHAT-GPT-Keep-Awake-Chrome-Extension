78fe0744f876a63ebc1d5feee8cd00b66e82f364
kennebecriver <39878271+kennebecriver@users.noreply.github.com>
2023-04-17 12:17:00 +0300

    Update background.js

--- background.js
+++ background.js
@@ -15,33 +15,21 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
             },
             func: () => console.log("Start")
         });
-        chrome.alarms.create("reloadTab", {
-            delayInMinutes: 0.8
-        });
+        chrome.alarms.create("reloadTab", { delayInMinutes: 0.3 });
     }
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-
-    if (alarm.name === "reloadTab") {
-        chrome.tabs.query({
-            currentWindow: true
-        }, (tabs) => {
-            for (let i = 0; i < tabs.length; i++) {
-                if (tabs[i].url.includes('https://chat.openai.com')) {
-                    chrome.scripting.executeScript({
-                        target: {
-                            tabId:tabs[i].id
-                        },
-                        func: () => fetch("https://chat.openai.com/backend-api/conversation", {"method": "GET","credentials": "include"})
-                            .then(e=>e.json())
-                            .then(console.log)
-                    });
-
-                    chrome.alarms.create("reloadTab", {delayInMinutes: 0.8});
-                    break
-                }
-            }
-        })
+    if (alarm.name !== "reloadTab") return
+    chrome.tabs.query({url: "https://chat.openai.com/*"}, tabs => {
+        if (!tabs.length) return
+        chrome.scripting.executeScript({
+            target: { tabId:tabs[0].id },
+            func: () => fetch("https://chat.openai.com/backend-api/conversation", {"method": "GET","credentials": "include"})
+                .then(e=>e.json())
+                .then(console.log)
+        });
+        chrome.alarms.create("reloadTab", { delayInMinutes: 0.8 });
+    })
     }
 });
